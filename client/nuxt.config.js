ab632cd60bc6a698ed3ca960af165b539b5c5768
kammradt <vinicius.kammradt1@gmail.com>
2019-06-17 20:48:51 -0300

    :lipstick: Adding css transitions

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -32,7 +32,7 @@ export default {
   /*
    ** Global CSS
    */
-  css: ['~/assets/style/app.styl'],
+  css: ['~/assets/style/app.styl', '~/assets/css/transitions.css'],
 
   /*
    ** Plugins to load before mounting the App
